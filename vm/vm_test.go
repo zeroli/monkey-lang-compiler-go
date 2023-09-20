7d52e6dc953a3a1c687162db7b283398db79d8c0
zero.li <zeroliql@gmail.com>
2023-09-19 22:13:06 -0700

    support arith op from vm

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -21,6 +21,24 @@ type vmTestCase struct {
 	expected interface{}
 }
 
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1+2", 3},
+		{"1-2", -1},
+		{"1*2", 2},
+		{"4/2", 2},
+		{"50/2 * 2 + 10 - 5", 55},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2*2*2*2*2", 32},
+		{"5*2 + 10", 20},
+		{"5 +2 * 10", 25},
+		{"5 * (2+10)", 60},
+	}
+	runVmTests(t, tests)
+}
+
 func runVmTests(t *testing.T, tests []vmTestCase) {
 	t.Helper()
 
@@ -68,13 +86,4 @@ func testIntegerObject(expected int64, actual object.Object) error {
 	}
 
 	return nil
-}
-
-func TestIntegerArithmetic(t *testing.T) {
-	tests := []vmTestCase{
-		{"1", 1},
-		{"2", 2},
-		{"1+2", 3},
-	}
-	runVmTests(t, tests)
 }
