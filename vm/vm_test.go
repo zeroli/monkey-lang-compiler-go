014fc3286985f6b463a49ea0a88082a1f296b75a
zero.li <zeroliql@gmail.com>
2023-09-24 23:21:53 -0700

    add more test for funtion call with return

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -168,6 +168,43 @@ func TestCallingFunctionWithoutArguments(t *testing.T) {
 			`,
 			expected: 15,
 		},
+		{
+			input: `
+			let one = fn() { 1; };
+			let two = fn() { 2; };
+			one() + two();
+			`,
+			expected: 3,
+		},
+		{
+			input: `
+			let a = fn() { 1 };
+			let b = fn() { a() + 1 };
+			let c = fn() { b() + 1 };
+			c();
+			`,
+			expected: 3,
+		},
+	}
+	runVmTests(t, tests)
+}
+
+func TestCallingFunctionWithReturnStatement(t *testing.T) {
+	tests := []vmTestCase{
+		{
+			input: `
+			let earlyExit = fn() { return 99; 100; };
+			earlyExit();
+			`,
+			expected: 99,
+		},
+		{
+			input: `
+			let earlyExit = fn() { return 99; return 100; };
+			earlyExit();
+			`,
+			expected: 99,
+		},
 	}
 	runVmTests(t, tests)
 }
