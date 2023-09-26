4e6021ac908b74d4b1d9035d40b0528a33eb92e9
zero.li <zeroliql@gmail.com>
2023-09-25 23:31:34 -0700

    put argument for call into local bindings

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -253,6 +253,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 		c.emit(code.OpIndex)
 	case *ast.FunctionLiteral:
 		c.enterScope()
+
+		// put arguments into local bindings
+		for _, p := range node.Parameters {
+			c.symbolTable.Define(p.Value)
+		}
+
 		err := c.Compile(node.Body)
 		if err != nil {
 			return err
