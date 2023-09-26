4c85a5ba973f0c6a4194e3c3b2b7f0233e656104
zero.li <zeroliql@gmail.com>
2023-09-26 00:20:22 -0700

    add builtins into object and change code to evaluator

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,51 +1,11 @@
 package evaluator
 
 import (
-	"fmt"
 	"monkey/object"
 )
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			case *object.Array:
-				return &object.Integer{Value: int64(len(arg.Elements))}
-			default:
-				return newError("argument to `len` not supported, got %s",
-					args[0].Type())
-			}
-		},
-	},
-	"push": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 2 {
-				return newError("wrong nubmer of argumetns. got=%d, want=2", len(args))
-			}
-			if args[0].Type() != object.ARRAY_OBJ {
-				return newError("argument to `push` must be ARRAY. got=%s", args[0].Type())
-			}
-
-			arr := args[0].(*object.Array)
-			length := len(arr.Elements)
-
-			newElements := make([]object.Object, length+1)
-			copy(newElements, arr.Elements)
-			newElements[length] = args[1]
-			return &object.Array{Elements: newElements}
-		},
-	},
-	"puts": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			for _, arg := range args {
-				fmt.Println(arg.Inspect())
-			}
-			return NULL
-		},
-	},
+	"len":  &object.GetBuiltinByName("len"),
+	"push": &object.GetBuiltinByName("push"),
+	"puts": &object.GetBuiltinByName("puts"),
 }
